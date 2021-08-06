e35291eba5879dd752a94e8994e839599c3b6752
KravchenkoAndrey24 <yazaandrey@gmail.com>
2021-08-06 13:19:19 +0300

    changed root route

--- src/api/API.ts
+++ src/api/API.ts
@@ -5,8 +5,8 @@ import { todoItemBodyType } from '../redux/todolistReducer';
 //     baseURL: 'http://localhost:3000/',
 // })
 
-axios.defaults.baseURL = 'http://localhost:3000';
-// axios.defaults.baseURL = 'https://ancient-beach-32575.herokuapp.com/';
+// axios.defaults.baseURL = 'http://localhost:3000';
+axios.defaults.baseURL = 'https://shrouded-caverns-92109.herokuapp.com';
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 
 const token = {
